41f1e194de89e3bf9e350e7d1163fe3c66973d40
David Rushton <david-rushton@outlook.com>
2020-12-10 01:00:00 +0000

    Day 9 star two

--- 2020/Day09/Program.cs
+++ 2020/Day09/Program.cs
@@ -12,6 +12,55 @@ namespace Day09
         {
             var testMode = args.Contains("--test");
             var inputPath = Path.Join(Directory.GetCurrentDirectory(), testMode ? "Test-Input.txt" : "Input.txt");
+
+            if(args.Length > 0)
+                if(long.TryParse(args[0], out var target))
+                    StarTwo(testMode, inputPath, target);
+
+            StarOne(testMode, inputPath);
+        }
+
+        static void StarTwo(bool testMode, string inputPath, long target)
+        {
+            long total = 0;
+            var queue = new Queue<long>();
+
+            foreach(var value in GetInput(inputPath))
+            {
+                total += value;
+                queue.Enqueue(value);
+
+                while(total > target)
+                {
+                    total -= queue.Dequeue();
+                }
+
+
+                if(total == target)
+                    ReportAndExit();
+            }
+
+
+            Console.WriteLine("Target not found\n:(");
+            Environment.Exit(1);
+
+
+            void ReportAndExit()
+            {
+                if(total == target && queue.Count > 1)
+                {
+                    var smallest = queue.Min();
+                    var largest = queue.Max();
+
+                    Console.WriteLine($"Target found!\n  Contiguous set:\n    Smallest number:{smallest}\n    Largest number: {largest}\n    Contiguous set size: {queue.Count}");
+                    Console.WriteLine($"Result: {smallest + largest}");
+                    Environment.Exit(0);
+                }
+            }
+        }
+
+        static void StarOne(bool testMode, string inputPath)
+        {
             var queueSize = testMode ? 5 : 25;
             var input = GetInput(inputPath);
             var queue = new Queue<long>(input.Take(queueSize));
