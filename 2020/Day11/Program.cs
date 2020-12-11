87abace3d42bc96f4ffe4eff5f1c4f99f6699ac6
David Rushton <david-rushton@outlook.com>
2020-12-11 14:29:18 +0000

    Day 11 boilerplate

---
+++ 2020/Day11/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Linq;
+
+
+namespace Day11
+{
+    class Program
+    {
+        static bool _verboseOutputOn = Environment.GetCommandLineArgs().Contains("--verbose");
+        static bool _useTestInput = Environment.GetCommandLineArgs().Contains("--test");
+        static string _inputPath = Path.Join
+        (
+                Directory.GetCurrentDirectory(),
+                _useTestInput ? "Test-Input.txt" : "Input.txt"
+        );
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+        static void VerboseWriteLine(string message)
+        {
+            if(_verboseOutputOn)
+                Console.WriteLine(message);
+        }
+    }
+}
