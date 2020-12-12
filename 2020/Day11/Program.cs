a60b3b474347892f7e91e074b455fdfe26acab5e
David Rushton <david-rushton@outlook.com>
2020-12-12 00:51:41 +0000

    DY 11 2nd star

--- 2020/Day11/Program.cs
+++ 2020/Day11/Program.cs
@@ -1,11 +1,24 @@
 ﻿using System;
 using System.Collections;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 
 
 namespace Day11
 {
+    public enum BoardDirection
+    {
+        Up,
+        UpRight,
+        Right,
+        DownRight,
+        Down,
+        DownLeft,
+        Left,
+        UpLeft
+    }
+
     public record Grid(
         char[][] Board,
         string FlatBoard,
@@ -33,24 +46,24 @@ namespace Day11
         static void Main(string[] args)
         {
             var lastGrid = GetInput();
-            var round = 0;
+            var round = 1;
 
-            VerboseWriteLine("Running simulation");
+            WriteLineVerbose("Running simulation");
 
             while(true)
             {
                 var nextGrid = GetNextRound(lastGrid);
 
-                VerboseWriteLine($"  Round {round++}");
+                WriteLineVerbose($"  Round {round++}");
+                WriteLineVerbose(nextGrid.Board);
 
                 if(lastGrid.FlatBoard == nextGrid.FlatBoard)
                 {
-                    VerboseWriteLine(nextGrid.Board);
                     Console.WriteLine
                     (
                         string.Format
                         (
-                            "\nGrid stabilised\nRound: {0}\nOccupied seat count: {1}",
+                            "\nGrid stabilised\nRound: {0}\nOccupied seat count: {1}\n",
                             round,
                             nextGrid.FlatBoard.Count(c => c == _seatOccupied)
                         )
@@ -163,34 +176,83 @@ namespace Day11
 
         static int CountOfOccupiedAdjacentSeatsExtended(Grid grid, int rowIndex, int colIndex)
         {
-            var startRow = rowIndex - 1 < 0 ? 0 : rowIndex - 1;
-            var endRow = rowIndex + 1 > grid.Rows - 1 ? grid.Rows - 1 : rowIndex + 1;
-            var startCol = colIndex - 1 < 0 ? 0 : colIndex - 1;
-            var endCol = colIndex + 1 > grid.Cols - 1 ? grid.Cols - 1 : colIndex + 1;
             var occupiedCount = 0;
 
-            for(var r = startRow; r <= endRow; r++)
+            foreach(var direction in (BoardDirection[])Enum.GetValues(typeof(BoardDirection)))
             {
-                for(var c = startCol; c <= endCol; c++)
-                {
-                    if(! (r == rowIndex && c == colIndex) )
-                        if(grid.Board[r][c] ==_seatOccupied)
-                            occupiedCount++;
-                }
+                occupiedCount += ReadFirstSeat(grid, direction, rowIndex, colIndex) == _seatOccupied ? 1 : 0;
             }
 
 
+            Debug.Assert(occupiedCount <= 8, $"Adjacent count cannot exceed the number of directions checks: {occupiedCount}");
             return occupiedCount;
+        }
+
+        static char ReadFirstSeat(Grid grid, BoardDirection direction, int row, int col)
+        {
+            var value = string.Empty;
+            int offset = direction switch
+            {
+                BoardDirection.Up           => grid.Cols * -1,
+                BoardDirection.UpRight      => (grid.Cols - 1) * -1,
+                BoardDirection.Right        => 1,
+                BoardDirection.DownRight    => grid.Cols + 1,
+                BoardDirection.Down         => grid.Cols,
+                BoardDirection.DownLeft     => grid.Cols - 1,
+                BoardDirection.Left         => -1,
+                BoardDirection.UpLeft       => (grid.Cols + 1) * -1,
+                _                           => -999
+            };
+            var lastIndex = ConvertAddressToIndex(grid.Rows - 1, grid.Cols - 1);
+            var index = ConvertAddressToIndex(row, col) + offset;
+            var address = (row, col);
+
+            while(index >= 0 && index <= lastIndex && isValidMove(direction, address.row, address.col))
+            {
+                address = ConvertIndexToAddress(index);
+                var cell = grid.Board[address.row][address.col];
+
+                if(isSeat(cell))
+                    return cell;
+
+                index += offset;
+            }
+
+
+            // If we didn't find a seat then the default value (_seatEmpty).
+            return _seatEmpty;
+
+
+            int ConvertAddressToIndex(int row, int col) => (row * (grid.Cols)) + col;
+
+            (int row, int col) ConvertIndexToAddress(int index) => (index / grid.Cols, index % grid.Cols);
+
+            bool isValidMove(BoardDirection direction, int rowIdx, int colIdx)
+            {
+                if(rowIdx == 0 && new [] {BoardDirection.Up, BoardDirection.UpRight, BoardDirection.UpLeft}.Contains(direction))
+                    return false;
+
+                if(colIdx == grid.Cols - 1 && new [] {BoardDirection.UpRight, BoardDirection.Right, BoardDirection.DownRight}.Contains(direction))
+                    return false;
+
+                if(rowIdx == grid.Rows -1 && new [] {BoardDirection.DownRight, BoardDirection.Down, BoardDirection.DownLeft}.Contains(direction))
+                    return false;
+
+                if(colIdx == 0 && new [] {BoardDirection.DownLeft, BoardDirection.Left, BoardDirection.UpLeft}.Contains(direction))
+                    return false;
+
+                return true;
+            }
         }
 
         static bool isSeat(char value) => value != _floor;
 
 
-        static void WriteLineVerbose(Grid grid) => VerboseWriteLine(grid.Board);
-        static void VerboseWriteLine(char[][] message) => VerboseWriteLine(string.Join('\n', message.Select(l => string.Join(string.Empty, l))));
-        static void VerboseWriteLine(char[] message) => VerboseWriteLine(string.Join(string.Empty, message));
-        static void VerboseWriteLine(char message) => VerboseWriteLine(message.ToString());
-        static void VerboseWriteLine(string message)
+        static void WriteLineVerbose(Grid grid) => WriteLineVerbose(grid.Board);
+        static void WriteLineVerbose(char[][] message) => WriteLineVerbose(string.Join('\n', message.Select(l => string.Join(string.Empty, l))));
+        static void WriteLineVerbose(char[] message) => WriteLineVerbose(string.Join(string.Empty, message.Append('\n')));
+        static void WriteLineVerbose(char message) => WriteLineVerbose(message.ToString());
+        static void WriteLineVerbose(string message)
         {
             if(_verboseOutputOn)
                 Console.WriteLine(message);
