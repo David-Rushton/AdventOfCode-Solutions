d3a3efd7a842dfbb8f061f5569815046b6dc5124
David Rushton <david-rushton@outlook.com>
2021-01-01 10:42:37 +0000

    Added 24 II!

--- 2020/Day24/Tiler.cs
+++ 2020/Day24/Tiler.cs
@@ -16,15 +16,15 @@ namespace AoC
 
             Console.WriteLine("\nLaying tiles:");
             foreach(var tile in tiles)
-                FlipFile(tile);
+                FlipTile(tile);
 
 
-            Console.WriteLine($"\nStats:\n  Tiles flipped: {tilesFlipped}\n  Black tiles: {blackTiles.Count}");
+            Console.WriteLine($"\nStats:\n  Tiles flipped: {tilesFlipped}\n  Black tiles: {blackTiles.Count}\n");
             Flip(blackTiles);
             return;
 
 
-            void FlipFile(TileToken tile)
+            void FlipTile(TileToken tile)
             {
                 if(blackTiles.ContainsKey(tile.Location))
                     blackTiles.Remove(tile.Location);
@@ -40,7 +40,7 @@ namespace AoC
         private void Flip(Dictionary<Location, TileToken> blackTiles)
         {
             Console.WriteLine("Simulating Days:");
-            for(var day = 1; day <= 10; day++)
+            for(var day = 1; day <= 100; day++)
             {
                 var whiteToFlip = WhiteTilesWithTwoBlackNeighbours(blackTiles).ToList();
                 var blackToFlip = BlackTilesWithZeroThreeOrMoreBlackNeigbours(blackTiles).ToList();
@@ -68,6 +68,30 @@ namespace AoC
             GetNeighbours(location).Where(n => blackTiles.ContainsKey(n)).Count()
         ;
 
+        private IEnumerable<Location> GetWhiteTiles(IEnumerable<Location> blackTiles)
+        {
+            var returnedLocations = new List<Location>();
+
+            // We only need to worry about white tiles that boarder a black tile.
+            // The rest of the floor cannot be flipped in the current day.
+            foreach(var blackTile in blackTiles)
+                foreach(var neighbour in GetNeighbours(blackTile))
+                    if( ! blackTiles.Contains(neighbour) )
+                        if(IsNewLocation(neighbour))
+                            yield return neighbour;
+
+
+            bool IsNewLocation(Location location)
+            {
+                if(returnedLocations.Contains(location))
+                    return false;
+
+
+                returnedLocations.Add(location);
+                return true;
+            }
+        }
+
         private IEnumerable<Location> GetNeighbours(Location location)
         {
             yield return new Location(location.Northing + 1, location.Easting + .5);    // ne
@@ -76,25 +100,6 @@ namespace AoC
             yield return new Location(location.Northing - 1, location.Easting - .5);    // sw
             yield return new Location(location.Northing,     location.Easting -  1);    // w
             yield return new Location(location.Northing + 1, location.Easting - .5);    // nw
-        }
-
-        private IEnumerable<Location> GetWhiteTiles(IEnumerable<Location> blackTiles)
-        {
-            var maxWest = Math.Floor(blackTiles.Min(t => t.Easting) - 1.0);
-            var maxEast = Math.Ceiling(blackTiles.Max(t => t.Easting) + 1.0);
-            var maxNorth = blackTiles.Min(t => t.Northing) - 1.0;
-            var maxSouth = blackTiles.Max(t => t.Northing) - 1.0;
-
-
-            for(var northing = maxSouth; northing <= maxNorth; northing++)
-                for(var easting = maxWest; easting <= maxEast; easting++)
-                {
-                    var offset = northing % 2 == 1 ? .5 : 0;
-                    var location = new Location(northing, easting + offset);
-
-                    if( ! blackTiles.Contains(location) )
-                        yield return location;
-                }
         }
     }
 }
