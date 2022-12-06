dc967ca378e652b6e4b6da772489139ffee174e6
David Rushton <david.rushton@trayport.com>
2022-12-06 14:30:43 +0000

    Adds day 6 star 2

--- 2022/day06.py
+++ 2022/day06.py
@@ -6,15 +6,23 @@ def main(isStarTwo: bool, streamPath: str):
     streams = getInput(streamPath)
 
     for stream in streams:
-        recentCharacters = ['', '', '', '']
+        last14 = ['', '', '', '', '', '', '', '', '', '', '', '', '', '']
+        last4 = ['', '', '', '']
         currentCharacter = 0
+        packetFound = False
+        messageFound = False
 
         for character in stream:
-            recentCharacters[currentCharacter % 4] = character
+            last4[currentCharacter % 4] = character
+            last14[currentCharacter % 14] = character
 
-            if(isStartOfPacket(recentCharacters)):
+            if not packetFound and isStartOfPacket(last4):
                 print (f'Start of packet <{stream[0:5]}...> found: {currentCharacter + 1}')
-                break
+                packetFound = True
+
+            if not messageFound and isStartOfMessage(last14):
+                print (f'Start of message <{stream[0:5]}...> found: {currentCharacter + 1}')
+                messageFound = True
 
             currentCharacter += 1
 
@@ -22,8 +30,22 @@ def main(isStarTwo: bool, streamPath: str):
 def isStartOfPacket(recentCharacters: list[str]):
     if recentCharacters[3] == '':
         return False
+
     uniqueCharacters = {recentCharacters[0], recentCharacters[1], recentCharacters[2], recentCharacters[3]}
     return len(uniqueCharacters) == 4
+
+def isStartOfMessage(recentCharacters: list[str]):
+    if recentCharacters[13] == '':
+        return False
+
+    uniqueCharacters = []
+    for character in recentCharacters:
+        if character in uniqueCharacters:
+            return False
+        else:
+            uniqueCharacters.append(character)
+
+    return True
 
 def getInput(path: str):
     for line in open(path, 'r').readlines():
