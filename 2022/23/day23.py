bbc3e434a7b3092e30e491ce8a45249fa8016b97
David Rushton <david-rushton@outlook.com>
2022-12-26 23:30:02 +0000

    Adds day 23
    Optimized for performance

--- 2022/23/day23.py
+++ 2022/23/day23.py
@@ -4,27 +4,22 @@ import sys
 import time
 
 @dataclass
-class Point:
-    x: int
-    y: int
-
-@dataclass
 class CandidateMove:
-    elf: Point
-    to: Point
+    elf: tuple[int, int]
+    to: tuple[int, int]
 
 ELF = '#'
 EMPTY_TILE = '.'
 ROUNDS = 10
 
-NORTH_OFFSET = Point(x=0, y=-1)
-NORTH_EAST_OFFSET = Point(x=1, y=-1)
-EAST_OFFSET = Point(x=1, y=0)
-SOUTH_EAST_OFFSET = Point(x=1, y=1)
-SOUTH_OFFSET = Point(x=0, y=1)
-SOUTH_WEST_OFFSET = Point(x=-1, y=1)
-WEST_OFFSET = Point(x=-1, y=0)
-NORTH_WEST_OFFSET = Point(x=-1, y=-1)
+NORTH_OFFSET = (0, -1)
+NORTH_EAST_OFFSET = (1, -1)
+EAST_OFFSET = (1, 0)
+SOUTH_EAST_OFFSET = (1, 1)
+SOUTH_OFFSET = (0, 1)
+SOUTH_WEST_OFFSET = (-1, 1)
+WEST_OFFSET = (-1, 0)
+NORTH_WEST_OFFSET = (-1, -1)
 
 NORTH_ISH_OFFSETS = [NORTH_OFFSET, NORTH_EAST_OFFSET, NORTH_WEST_OFFSET]
 SOUTH_ISH_OFFSETS = [SOUTH_OFFSET, SOUTH_EAST_OFFSET, SOUTH_WEST_OFFSET]
@@ -33,12 +28,11 @@ EAST_ISH_OFFSETS = [EAST_OFFSET, NORTH_EAST_OFFSET, SOUTH_EAST_OFFSET]
 
 
 def main(path: str) -> None:
-    elves = list(parse_elves(path))
+    elves = set(parse_elves(path))
     min_x = min_y = 0
     max_x = max_y = 15
     offset_groups = [NORTH_ISH_OFFSETS, SOUTH_ISH_OFFSETS, WEST_ISH_OFFSETS, EAST_ISH_OFFSETS]
     round = 0
-
     os.system('cls')
     print_map(elves, round, ['Elf starting positions'], min_x, max_x, min_y, max_y)
 
@@ -53,29 +47,27 @@ def main(path: str) -> None:
 
         # move
         for candidate_move in candidate_moves:
-            if candidate_points.count(candidate_move.to) == 1:
-                events.append(f'Elf moving from {candidate_move.elf.x}x{candidate_move.elf.y} -> {candidate_move.to.x}x{candidate_move.to.y}')
+            if candidate_points[(candidate_move.to[0], candidate_move.to[1])] == 1:
+                events.append(f'Elf moving from {candidate_move.elf[0]}x{candidate_move.elf[1]} -> {candidate_move.to[0]}x{candidate_move.to[1]}')
 
-                # don't replace elf
-                # update; so the change is respected in the elves collection.
-                candidate_move.elf.x = candidate_move.to.x
-                candidate_move.elf.y = candidate_move.to.y
+                elves.remove(candidate_move.elf)
+                elves.add(candidate_move.to)
 
-                if candidate_move.to.x < min_x:
-                    min_x = candidate_move.to.x
+                if candidate_move.to[0] < min_x:
+                    min_x = candidate_move.to[0]
 
-                if candidate_move.to.x > max_x:
-                    max_x = candidate_move.to.x
+                if candidate_move.to[0] > max_x:
+                    max_x = candidate_move.to[0]
 
-                if candidate_move.to.y < min_y:
-                    min_y = candidate_move.to.y
+                if candidate_move.to[1] < min_y:
+                    min_y = candidate_move.to[1]
 
-                if candidate_move.to.y > max_y:
-                    max_y = candidate_move.to.y
+                if candidate_move.to[1] > max_y:
+                    max_y = candidate_move.to[1]
 
                 elves_moved += 1
             else:
-                events.append(f'Elf not moving from {candidate_move.to.x}x{candidate_move.to.y}')
+                events.append(f'Elf not moving from {candidate_move.to[0]}x{candidate_move.to[1]}')
 
         if elves_moved == 0:
             break
@@ -87,60 +79,65 @@ def main(path: str) -> None:
     min_x, min_y, max_x, max_y = get_enclosing_retangle(elves)
     print_map(elves, round, events, min_x, max_x, min_y, max_y)
 
-def get_enclosing_retangle(elves: list[Point]) -> tuple[int, int, int, int]:
+def get_enclosing_retangle(elves: set[tuple[int, int]]) -> tuple[int, int, int, int]:
     min_x = min_y = max_x = max_y = 0
     for elf in elves:
-        if elf.x < min_x:
-            min_x = elf.x
+        if elf[0] < min_x:
+            min_x = elf[0]
 
-        if elf.x > max_x:
-            max_x = elf.x
+        if elf[0] > max_x:
+            max_x = elf[0]
 
-        if elf.y < min_y:
-            min_y = elf.y
+        if elf[1] < min_y:
+            min_y = elf[1]
 
-        if elf.y > max_y:
-            max_y = elf.y
+        if elf[1] > max_y:
+            max_y = elf[1]
 
     return min_x, min_y, max_x, max_y
 
-def get_candidates(elves: list[Point], offset_groups = list[list[Point]]) -> tuple[list[CandidateMove], dict[Point, int]]:
+def get_candidates(elves: set[tuple[int, int]], offset_groups = list[list[tuple[int, int]]]) -> tuple[list[CandidateMove], set[tuple[int, int]]]:
     candidate_moves = []
-    candidate_points = []
+    candidate_points = {}
 
     for elf in elves:
-        if has_empty_neighbours(elves, elf):
+        neighbours = list(get_neighbours(elf, elves))
+
+        if len(neighbours) == 0:
             continue
 
         for offsets in offset_groups:
-            if has_empty_offsets(elves, elf, offsets):
+            if has_empty_offsets(neighbours, elf, offsets):
                 candidate_point = get_offset(elf, offsets[0])
-                candidate_points.append(candidate_point)
+
+                if candidate_point in candidate_points:
+                    candidate_points[candidate_point] += 1
+                else:
+                    candidate_points[candidate_point] = 1
+
                 candidate_moves.append(CandidateMove(elf=elf, to=candidate_point))
                 break
 
     return candidate_moves, candidate_points
 
-def has_empty_offsets(elves: list[Point], starting_point: Point, offsets: list[Point]) -> bool:
+def get_neighbours(elf: tuple[int, int], elves: set[tuple[int, int]]):
+    for y in (-1, 0, 1):
+        for x in (-1, 0 , 1):
+            if not (y == 0 and x == 0):
+                if (elf[0] + x, elf[1] + y) in elves:
+                    yield (elf[0] + x, elf[1] + y)
+
+def has_empty_offsets(elves: set[tuple[int, int]], starting_point: tuple[int, int], offsets: list[tuple[int, int]]) -> bool:
     for offset in offsets:
         if get_offset(starting_point, offset) in elves:
             return False
 
     return True
 
-def has_empty_neighbours(elves: list[Point], starting_point: Point) -> bool:
-    for y_offset in (-1, 0, 1):
-        for x_offset in (-1, 0, 1):
-            if not (y_offset == 0 and x_offset == 0):
-                if get_offset(starting_point, Point(x_offset, y_offset)) in elves:
-                    return False
+def get_offset(starting_point: tuple[int, int], offset: tuple[int, int]) -> tuple[int, int]:
+    return (starting_point[0] + offset[0], starting_point[1] + offset[1])
 
-    return True
-
-def get_offset(starting_point: Point, offset: Point) -> Point:
-    return Point(x=starting_point.x + offset.x, y=starting_point.y + offset.y)
-
-def print_map(elves: list[Point], round: int, events: list[str], min_x: int, max_x: int, min_y: int, max_y: int):
+def print_map(elves: set[tuple[int, int]], round: int, events: list[str], min_x: int, max_x: int, min_y: int, max_y: int):
     os.system('cls')
     print('\033[1;1H', end='')
     print('\n== Unstable Diffusion ==')
@@ -151,7 +148,7 @@ def print_map(elves: list[Point], round: int, events: list[str], min_x: int, max
     for y in range(min_y, max_y + 1):
         row = ''
         for x in range(min_x, max_x + 1):
-            if Point(x, y) in elves:
+            if (x, y) in elves:
                 row += ELF
             else:
                 row += EMPTY_TILE
@@ -165,9 +162,9 @@ def print_map(elves: list[Point], round: int, events: list[str], min_x: int, max
     #     # print('\nPress any key to continue\n')
     #     input()
 
-    time.sleep(.5)
+    # time.sleep(.5)
 
-def parse_elves(path: str) -> list[Point]:
+def parse_elves(path: str) -> set[tuple[int, int]]:
     y = -1
     for line in open(path, 'r').read().splitlines():
         y += 1
@@ -175,7 +172,7 @@ def parse_elves(path: str) -> list[Point]:
         for char in line:
             x += 1
             if char == ELF:
-                yield Point(x, y)
+                yield (x, y)
 
 
 if __name__ == '__main__':
