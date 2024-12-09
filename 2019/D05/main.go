5000b22335c102471876a413b2e3e935103fd2d3
David Rushton <david.rushton@trayport.com>
2024-12-09 14:45:47 +0000

    Adds 2029 D5-WIP

--- 2019/D05/main.go
+++ 2019/D05/main.go
@@ -4,39 +4,63 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
+
+var (
+	testMode bool
+	star     int
+)
+
+func init() {
+	if slices.Contains(os.Args, "-t") {
+		testMode = true
+	}
+
+	star = 1
+	if slices.Contains(os.Args, "-2") {
+		star = 2
+	}
+}
 
 func main() {
 	fmt.Println("--- Day 5: Sunny with a Chance of Asteroids ---")
 	fmt.Println()
 
-	data, err := os.ReadFile("./input.test.txt")
-	if err != nil {
-		log.Fatalf("Cannot read file because %v.", err)
-	}
+	for _, program := range parse() {
 
-	for _, line := range strings.Split(string(data), "\r\n") {
-		result := compute(parse(line))
-		fmt.Println(result)
-	}
+		if !testMode {
+			if star == 1 {
+				overrideProgram := make([]int64, len(program))
+				copy(overrideProgram, program)
+				overrideProgram[1] = 12
+				overrideProgram[2] = 2
 
-}
+				fmt.Printf("Program: %v = %d \n", overrideProgram, compute(overrideProgram))
+				return
+			}
 
-func parse(input string) []int64 {
-	result := []int64{}
+			for left := int64(1); left < 99; left++ {
+				for right := int64(1); right < 99; right++ {
+					overrideProgram := make([]int64, len(program))
+					copy(overrideProgram, program)
+					overrideProgram[1] = left
+					overrideProgram[2] = right
 
-	for _, item := range strings.Split(input, ",") {
-		i, err := strconv.ParseInt(item, 10, 64)
-		if err != nil {
-			log.Fatalf("Cannot parse int from value -%v-.", item)
+					fmt.Printf("Program: %v...\n", overrideProgram[0:10])
+
+					if compute(overrideProgram) == 19690720 {
+						fmt.Printf("%d%d\n", left, right)
+						return
+					}
+				}
+
+			}
 		}
 
-		result = append(result, i)
 	}
-
-	return result
 }
 
 func compute(program []int64) int64 {
@@ -70,4 +94,39 @@ func compute(program []int64) int64 {
 	// ut oh
 	log.Fatalf("This is not my beautiful house")
 	return -1
+}
+
+func parse() [][]int64 {
+	path := "input.txt"
+	if testMode {
+		path = "input.test.txt"
+	}
+
+	data, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatalf("Cannot read file because %v.", err)
+	}
+
+	result := [][]int64{}
+
+	for _, line := range strings.Split(string(data), "\r\n") {
+		if line != "" {
+			program := []int64{}
+
+			for _, item := range strings.Split(line, ",") {
+				if item != "" {
+					i, err := strconv.ParseInt(item, 10, 64)
+					if err != nil {
+						log.Fatalf("Cannot parse int from value -%v-.", item)
+					}
+
+					program = append(program, i)
+				}
+			}
+
+			result = append(result, program)
+		}
+	}
+
+	return result
 }
