9dfd81a03469231c3b4c6edc89104ff9808afa80
David Rushton <david-rushton@outlook.com>
2024-12-24 21:39:33 +0000

    Adds D24S1

--- 2024/cmd/day21/main.go
+++ 2024/cmd/day21/main.go
@@ -2,47 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math"
+	"strconv"
+	"strings"
 
 	"github.com/David-Rushton/AdventOfCode-Solutions/tree/main/2024/internal/aoc"
 )
-
-/*
-      0   1   2
-	+---+---+---+
-	| 7 | 8 | 9 | 0
-	+---+---+---+
-	| 4 | 5 | 6 | 1
-	+---+---+---+
-	| 1 | 2 | 3 | 2
-	+---+---+---+
-		| 0 | A | 3
-		+---+---+
-
-      0   1   2
-		+---+---+
-		| ^ | A | 10
-	+---+---+---+
-	| < | v | > | 11
-	+---+---+---+
-
-*/
-
-type directedPoint struct {
-	direction rune
-	point     point
-}
 
 type point struct {
 	x int
 	y int
 }
 
-type keypad map[rune]point
-type points map[point]bool
+type directedPoint struct {
+	direction rune
+	point     point
+}
 
 var (
-	numericKeypad = keypad{
+	numericKeypad = map[rune]point{
 		'7': {x: 0, y: 0},
 		'8': {x: 1, y: 0},
 		'9': {x: 2, y: 0},
@@ -55,32 +34,32 @@ var (
 		'0': {x: 1, y: 3},
 		'A': {x: 2, y: 3},
 	}
-	numericPoints = points{
-		{x: 0, y: 0}: true,
-		{x: 1, y: 0}: true,
-		{x: 2, y: 0}: true,
-		{x: 0, y: 1}: true,
-		{x: 1, y: 1}: true,
-		{x: 2, y: 1}: true,
-		{x: 0, y: 2}: true,
-		{x: 1, y: 2}: true,
-		{x: 2, y: 2}: true,
-		{x: 1, y: 3}: true,
-		{x: 2, y: 3}: true,
+	numericPoints = map[point]rune{
+		{x: 0, y: 0}: '7',
+		{x: 1, y: 0}: '8',
+		{x: 2, y: 0}: '9',
+		{x: 0, y: 1}: '4',
+		{x: 1, y: 1}: '5',
+		{x: 2, y: 1}: '6',
+		{x: 0, y: 2}: '1',
+		{x: 1, y: 2}: '2',
+		{x: 2, y: 2}: '3',
+		{x: 1, y: 3}: '0',
+		{x: 2, y: 3}: 'A',
 	}
-	directionKeypad = keypad{
+	directionalKeypad = map[rune]point{
 		'^': {x: 1, y: 10},
 		'A': {x: 2, y: 10},
 		'<': {x: 0, y: 11},
 		'v': {x: 1, y: 11},
 		'>': {x: 2, y: 11},
 	}
-	directionPoints = points{
-		{x: 1, y: 10}: true,
-		{x: 2, y: 10}: true,
-		{x: 0, y: 11}: true,
-		{x: 1, y: 11}: true,
-		{x: 2, y: 11}: true,
+	directionalPoints = map[point]rune{
+		{x: 1, y: 10}: '^',
+		{x: 2, y: 10}: 'A',
+		{x: 0, y: 11}: '<',
+		{x: 1, y: 11}: 'v',
+		{x: 2, y: 11}: '>',
 	}
 )
 
@@ -88,103 +67,218 @@ func main() {
 	fmt.Println("--- Day 21: Keypad Conundrum ---")
 	fmt.Println()
 
-	for _, code := range aoc.Input {
-		fmt.Printf("  %v\n", code)
+	robots := 2
+	if aoc.Star == aoc.StarTwo {
+		robots = 25
+	}
 
-		from := 'A'
-		for _, to := range code {
-			fmt.Printf("    %v\n", string(to))
+	var complexity int
 
-			for _, route := range getNumericRoute(from, to) {
-				fmt.Printf("      %v\n", route)
+	codes := aoc.Input
+	for _, code := range codes {
+		shortestSequenceLen := math.MaxInt
+		outerSequences := getNumericSequences(code)
 
-				srFrom := 'A'
-				for _, srTo := range route {
+		for i := 1; i <= robots; i++ {
+			nextOuterSequences := make(map[string]int)
 
-					for _, subRoute := range getDirectionRoute(srFrom, srTo) {
-						fmt.Printf("         %v\n", subRoute)
-
-						ssrFrom := 'A'
-						for _, srrTo := range subRoute {
-							for _, subSubRoute := range getDirectionRoute(ssrFrom, srrTo) {
-								fmt.Printf("          %v\n", subSubRoute)
-							}
-						}
-					}
-
-					srFrom = srTo
+			for _, outerSequence := range outerSequences {
+				for _, innerSequence := range getDirectionalSequences(outerSequence) {
+					nextOuterSequences[innerSequence]++
 				}
 			}
 
-			from = to
+			outerSequences = []string{}
+			for sequence := range nextOuterSequences {
+				outerSequences = append(outerSequences, sequence)
+			}
 		}
 
-		fmt.Println()
+		for _, sequence := range outerSequences {
+			if len(sequence) < shortestSequenceLen {
+				shortestSequenceLen = len(sequence)
+			}
+		}
+
+		codeValue, err := strconv.ParseInt(code[0:3], 10, 64)
+		if err != nil {
+			log.Fatalf("Cannot convert %v to a number.", code[0:3])
+		}
+
+		fmt.Printf("  - %v | % 7d * % 4d = %v\n", code, shortestSequenceLen, codeValue, shortestSequenceLen*int(codeValue))
+		complexity += shortestSequenceLen * int(codeValue)
 	}
 
 	fmt.Println()
-	fmt.Printf("Results: %d\n", -1)
+	fmt.Printf("Complexity : %d", complexity)
 }
 
-func getNumericRoute(from, to rune) []string {
-	return getRoutes(numericKeypad, numericPoints, from, to)
+func getNumericSequences(code string) []string {
+	return getSequences(code, numericPoints, numericKeypad)
 }
 
-func getDirectionRoute(from, to rune) []string {
-	return getRoutes(directionKeypad, directionPoints, from, to)
-}
-
-func getRoutes(keypad keypad, points points, from, to rune) []string {
+func getDirectionalSequences(code string) []string {
 	type state struct {
-		point     point
-		score     int
-		direction rune
-		path      string
+		position int
+		path     string
 	}
 
+	sequences := strings.Split(code, "A")
+	sequences = sequences[0 : len(sequences)-1]
+
+	queue := []state{{position: 0, path: ""}}
+	paths := make(map[string]int)
+	shortestPath := math.MaxInt
+
+	for len(queue) > 0 {
+		current := queue[0]
+		queue = queue[1:]
+
+		if len(current.path) > shortestPath {
+			continue
+		}
+
+		if current.position >= len(sequences) {
+			if len(current.path) <= shortestPath {
+				shortestPath = len(current.path)
+				paths[current.path]++
+			}
+
+			continue
+		}
+
+		for _, sequence := range getSequences(sequences[current.position]+"A", directionalPoints, directionalKeypad) {
+			queue = append(queue, state{
+				position: current.position + 1,
+				path:     current.path + sequence})
+		}
+	}
+
+	result := []string{}
+	for k := range paths {
+		result = append(result, k)
+	}
+
+	return result
+}
+
+var sequenceCache = make(map[string][]string)
+
+func getSequences(code string, points map[point]rune, keypad map[rune]point) []string {
+	type state struct {
+		position int
+		from     rune
+		path     string
+	}
+
+	// Use cached results if possible.
+	if _, found := sequenceCache[code]; found {
+		return sequenceCache[code]
+	}
+
+	// Find values.
 	queue := []state{{
-		point:     keypad[from],
-		score:     0,
-		direction: 'X',
-		path:      ""}}
-	visited := make(map[point]bool)
-	bestScore := math.MaxInt
+		position: 0,
+		from:     'A',
+		path:     "",
+	}}
+
+	shortestResult := math.MaxInt
 	result := []string{}
 
 	for len(queue) > 0 {
 		current := queue[0]
 		queue = queue[1:]
 
-		if current.point == keypad[to] {
-			if current.score < bestScore {
-				bestScore = current.score
-				result = append(result, current.path+"A")
+		if len(current.path) > shortestResult {
+			continue
+		}
+
+		if current.position == len(code) {
+			if len(current.path) <= shortestResult {
+				shortestResult = len(current.path)
+				result = append(result, current.path)
 			}
 			continue
 		}
 
-		for _, neighbour := range getNeighbours(current.point) {
-			if points[neighbour.point] && !visited[neighbour.point] {
-				score := 1
-				if current.direction != neighbour.direction {
-					score = 100
-				}
+		to := rune(code[current.position])
+		for _, movement := range getMovements(points, keypad[current.from], keypad[to]) {
+			queue = append(queue, state{
+				position: current.position + 1,
+				from:     to,
+				path:     current.path + movement})
+		}
+	}
 
-				queue = append(queue, state{
-					neighbour.point,
-					score,
-					neighbour.direction,
-					current.path + string(neighbour.direction)})
+	// Cache result.
+	sequenceCache[code] = result
+
+	return result
+}
+
+type movementKey struct {
+	from point
+	to   point
+}
+
+var movementCache = make(map[movementKey][]string)
+
+func getMovements(points map[point]rune, from, to point) []string {
+	cacheKey := movementKey{from, to}
+	if _, found := movementCache[cacheKey]; found {
+		return movementCache[cacheKey]
+	}
+
+	type queueState struct {
+		point point
+		steps int
+		route string
+	}
+
+	queue := []queueState{{from, 0, ""}}
+	visited := make(map[point]bool)
+	fewestSteps := math.MaxInt
+	result := []string{}
+
+	for len(queue) > 0 {
+		current := queue[0]
+		queue = queue[1:]
+
+		if current.steps > fewestSteps {
+			continue
+		}
+
+		if current.point == to && current.steps <= fewestSteps {
+			result = append(result, current.route+"A")
+			fewestSteps = current.steps
+		}
+
+		for _, offset := range getOffsets(current.point) {
+			if _, found := points[offset.point]; found {
+				if _, found := visited[offset.point]; !found {
+					steps := 1
+					if len(current.route) > 0 && current.route[len(current.route)-1] != byte(offset.direction) {
+						steps = 10
+					}
+
+					queue = append(queue, queueState{
+						offset.point,
+						current.steps + steps,
+						current.route + string(offset.direction)})
+				}
 			}
 		}
 
 		visited[current.point] = true
 	}
 
+	movementCache[cacheKey] = result
+
 	return result
 }
 
-func getNeighbours(from point) []directedPoint {
+func getOffsets(from point) []directedPoint {
 	return []directedPoint{
 		{'^', point{from.x + 0, from.y - 1}},
 		{'>', point{from.x + 1, from.y + 0}},
