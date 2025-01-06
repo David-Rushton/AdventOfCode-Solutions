3a3e9e521d2b45d227475a832b8c7bf954fdac57
David Rushton <david.rushton@trayport.com>
2025-01-06 18:42:59 +0000

    Adds 2015 D6S1+2

---
+++ 2015/day07/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/David-Rushton/AdventOfCode-Solutions/tree/main/2015/internal/aoc"
+)
+
+func main() {
+	fmt.Println("--- Day 7: Some Assembly Required ---")
+	fmt.Println()
+
+	parse(aoc.GetInput(7))
+
+	fmt.Println()
+	fmt.Printf("Result: %d\n", -1)
+}
+
+func parse(input []string) {
+}
