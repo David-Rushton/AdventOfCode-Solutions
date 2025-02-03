fe229c8dbdcd418a066be58eff3b0becfa148a55
David Rushton <david.rushton@trayport.com>
2025-02-03 18:38:01 +0000

    Adds Y16 D02 S1+2

--- 2016/day01/main.go
+++ 2016/day01/main.go
@@ -13,20 +13,27 @@ type instruction struct {
 	steps int
 }
 
+type point struct {
+	x int
+	y int
+}
+
 func main() {
 	fmt.Println("--- Day 1: No Time for a Taxicab ---")
 	fmt.Println()
 
 	instructions := parse(aoc.GetInput(1)[0])
-	result := getManhattanDistance(instructions)
+	distance, firstRevisit := solve(instructions)
 
 	fmt.Println()
-	fmt.Printf("Result: %d\n", result)
+	fmt.Printf("Manhattan distance: %d\n", distance)
+	fmt.Printf("First Revisit: %d\n", firstRevisit)
 }
 
-func getManhattanDistance(instructions []instruction) int {
-	var x int
-	var y int
+func solve(instructions []instruction) (distance, firstRevisit int) {
+	firstRevisit = 0
+	location := point{x: 0, y: 0}
+	visited := map[point]int{}
 	direction := 0
 
 	for _, instruction := range instructions {
@@ -42,31 +49,38 @@ func getManhattanDistance(instructions []instruction) int {
 			}
 		}
 
-		switch direction % 4 {
-		// north
-		case 0:
-			y -= instruction.steps
+		for ; instruction.steps > 0; instruction.steps-- {
+			switch direction % 4 {
+			// north
+			case 0:
+				location.y -= 1
 
-		// east
-		case 1:
-			x += instruction.steps
+			// east
+			case 1:
+				location.x += 1
 
-		// south
-		case 2:
-			y += instruction.steps
+			// south
+			case 2:
+				location.y += 1
 
-		// west
-		case 3:
-			x -= instruction.steps
+			// west
+			case 3:
+				location.x -= 1
 
-		default:
-			panic(fmt.Sprintf("Unsupported direction: %d", direction))
+			default:
+				panic(fmt.Sprintf("Unsupported direction: %d", direction))
+			}
+
+			visited[point{location.x, location.y}]++
+			if visited[point{location.x, location.y}] == 2 && firstRevisit == 0 {
+				firstRevisit = int(math.Abs(float64(location.x)) + math.Abs(float64(location.y)))
+			}
 		}
 
-		fmt.Printf(" - Location: %d x %d\n", x, y)
+		fmt.Printf(" - Location: %d x %d\n", location.x, location.y)
 	}
 
-	return int(math.Abs(float64(x)) + math.Abs(float64(y)))
+	return int(math.Abs(float64(location.x)) + math.Abs(float64(location.y))), firstRevisit
 }
 
 func parse(input string) []instruction {
